01cd1cd6e45cf46ecf813121c304cba49ffb31ba
tonton194 <118404353+tonton194@users.noreply.github.com>
2022-12-12 08:38:29 +0900

    Add files via upload

---
+++ dinner.js
@@ -0,0 +1,34 @@
+'use strict';
+const dinnerNameInput = document.getElementById('dinnerName');
+const dinnerbuton = document.getElementById('choice');
+const dinnerdiv = document.getElementById('dinner-eria');
+const tweetdiv = document.getElementById('tweet-eria');
+
+dinnerbuton.onclick = () => {
+    const dinner = dinnerNameInput.value;
+    console.log(dinner);
+    dinnerdiv.innerText = ' ';
+    let food = document.createElement('h3');
+    food.innerText = '今日の晩御飯';
+    dinnerdiv.appendChild(food);
+
+    let food1 = document.createElement('p');
+    let yuushoku = gohan(dinner);
+    food1.innerText = yuushoku;
+    dinnerdiv.appendChild(food1);
+};
+let foodchoice = {
+    辛い:['麻婆豆腐','カレー','チゲ鍋','担々麺','豚キムチ','エビチリ','トッポギ'],
+    麺類:['ラーメン','皿うどん','とろろそば','月見うどん','チャンポン','月見そば','ナポリタン','ペペロンチーノ'],
+    肉料理:['ステーキ','ハンバーグ','照り焼きチキン']
+};
+
+function gohan(dinner){
+    console.log(dinner);
+    let hairetu = foodchoice[dinner];
+    console.log(hairetu);
+    let junban = Math.floor(Math.random()*(hairetu.length));
+    let yuushoku = hairetu[junban];
+    return yuushoku;
+};
+
